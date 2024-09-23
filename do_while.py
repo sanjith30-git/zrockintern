c3a8532cd069e6a975ed4e54b61e6ef7bed9f078
sanjith30-git <sagayasanjith30@gmail.com>
2024-09-23 21:11:22 +0530

    Create do_while.py

---
+++ do_while.py
@@ -0,0 +1,11 @@
+# Function to print numbers from 1 to 10 using a do-while loop equivalent
+def print_numbers_do_while():
+    i = 1
+    while True:
+        print(i)
+        i += 1
+        if i > 10:
+            break
+
+# Calling the function
+print_numbers_do_while()
